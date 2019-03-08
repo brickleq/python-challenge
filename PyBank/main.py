e9ffbddd0a1ea7ec7b1eecce1f44bd3535f4feaf
Brickey LeQuire <pbl@uchicago.edu>
2019-03-07 21:11:59 -0600

    second commit--first homework assignment complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -20,46 +20,49 @@ max_increase = 0 # greatest month-to-month increase, in $
 max_decrease = 0 # greatest month-t0-month decrease, in $
 max_increase_month = () # month and year of max_increase
 max_decrease_month = () # month and year of max_decrease
-first_loop = True 
+first_loop = True # set to false after first row of CSV file is read
 
 
 with open(csvpath, 'r', newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    csv_header = next(csvreader) #skip header row
+    csv_header = next(csvreader) # skip header row
     for row in csvreader:
-        date.append(row[0]) #populate list of dates
-        pl.append(row[1]) #populate list of profits/losses
-        if first_loop == False:
-            pl_change = float(row[1]) - pl_prev_row
-            pl_change_list.append(pl_change)
-        pl_total = pl_total + float(row[1]) #keep running total of net profits/losses
-        pl_prev_row = float(row[1])
-        if first_loop == True:
-            first_loop = False
+        date.append(row[0]) # populate list of dates
+        pl.append(row[1]) # populate list of profits/losses
+        if first_loop == False: # once we've read two rows of data...
+            pl_change = float(row[1]) - pl_prev_row # calculate difference between current month and previous
+            pl_change_list.append(pl_change) # and store in a list
+        pl_total = pl_total + float(row[1]) # keep running total of net profits/losses
+        pl_prev_row = float(row[1]) # next month, this month will be last month
+        if first_loop == True: # the first loop is followed by...
+            first_loop = False # a bunch of not-first loops
 
-months_total=len(date) #number of months in CSV file
-avg_change = statistics.mean(pl_change_list)
-max_increase = max(pl_change_list)
-max_decrease = min(pl_change_list)
-max_increase_index = pl_change_list.index(max_increase)
-max_decrease_index = pl_change_list.index(max_decrease)
-max_increase_month = date[(max_increase_index+1)]
+months_total=len(date) # number of months in CSV file
+avg_change = statistics.mean(pl_change_list) # caluclate average monthly change
+max_increase = max(pl_change_list) # determine greatest monthly increase
+max_decrease = min(pl_change_list) # determine greatest monthly decrease
+max_increase_index = pl_change_list.index(max_increase) # get index number
+max_decrease_index = pl_change_list.index(max_decrease) 
+max_increase_month = date[(max_increase_index+1)] # use index number to identify month/year
 max_decrease_month = date[(max_decrease_index+1)]
 
-
-
-# print(pl_change_list)
-
+# output results to screen
 print()
 print('Financial Analysis')
 print('----------------------------')
 print('Total Months: '+str(months_total))
 print('Total: $'+'{:,.2f}'.format(pl_total))
 print('Average Change: $'+'{:,.2f}'.format(avg_change))
-
 print('Greatest Increase in Profits: $'+'{:,.2f}'.format(max_increase)+' ('+max_increase_month+')')
 print('Greatest Decrease in Profits: $'+'{:,.2f}'.format(max_decrease)+' ('+max_decrease_month+')') 
 
-
-
-
+# output results to file
+text_file = open("PyBank_output_PBL.txt", "w")
+text_file.write('Financial Analysis\n')
+text_file.write('----------------------------\n')
+text_file.write('Total Months: '+str(months_total)+'\n')
+text_file.write('Total: $'+'{:,.2f}'.format(pl_total)+'\n')
+text_file.write('Average Change: $'+'{:,.2f}'.format(avg_change)+'\n')
+text_file.write('Greatest Increase in Profits: $'+'{:,.2f}'.format(max_increase)+' ('+max_increase_month+')\n')
+text_file.write('Greatest Decrease in Profits: $'+'{:,.2f}'.format(max_decrease)+' ('+max_decrease_month+')\n') 
+text_file.close()
